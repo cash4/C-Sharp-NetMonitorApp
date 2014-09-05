97d93dc8d411efb1a17722da4fb825c64c906cbd
Corey Ash <corey.j.ash@gmail.com>
2014-09-05 08:27:22 -0400

    Test of Git
    Test of Git

--- NetMonitorApp/NetMonitorApp/Program.cs
+++ NetMonitorApp/NetMonitorApp/Program.cs
@@ -407,6 +407,7 @@ namespace NetMonitorApp
                 while (retries-- > 0 && !pingSuccess)
                 {
                     //AppendToLog("VPN Ping reply status=" + pingReply.Status.ToString());
+                    //Test of Git
                     switch (pingReply.Status)
                     {
                         case System.Net.NetworkInformation.IPStatus.Success:
